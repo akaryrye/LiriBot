cad00c532dd4f76e2a5b1953cc73e1a9d5129d4e
akaryrye <ralldrin@gmail.com>
2019-08-16 20:34:33 -0700

    basic axios request for OMDB

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('this is a test');
+
+const axios = require('axios');
+
+const omdbURL = "http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy"
+
+axios.get(omdbURL)
+   .then(function (response) {
+      console.log("The movie's rating is: " + response.data.imdbRating);
+   })
+   .catch(function (err) {
+      if (err.response) {
+         console.log(err.response.status);
+      } else if (err.request) {
+         console.log(err.request);
+      } else {
+         // Something happened in setting up the request that triggered an Error
+         console.log("Error", err.message);
+      }
+      console.log(err.config);
+   });
